be0edaa808396805cdfc3289dc1897a4cd88069d
Stephen Roebuck <shroebuck@gmail.com>
2025-05-01 12:46:11 -0700

    init puckdle react app

--- amplify-app/src/index.js
+++ amplify-app/src/index.js
@@ -1,35 +1,10 @@
-import { Amplify } from 'aws-amplify';
-import { signIn, signOut, fetchAuthSession } from 'aws-amplify/auth';
-import awsExports from './aws-exports'; // Import your aws-exports.js file
+import React from 'react';
+import CheldleApp from './App'; // Adjust the path if needed
 
-//  VERY IMPORTANT:  Configure Amplify *before* using it.
-Amplify.configure(awsExports); // Use the configuration from awsExports
+const App = () => {
+    return (
+        <CheldleApp />
+    );
+};
 
-async function handleSignIn() {
-  try {
-      const user = await signIn({
-          username: 'testuser', //  email or username
-          password: 'testpassword',
-      });
-      console.log('Sign in successful:', user);
-
-      // Example of fetching auth session
-      const session = await fetchAuthSession();
-      console.log("Session:", session);
-
-
-  } catch (error) {
-      console.error('Error signing in:', error);
-  }
-}
-
-async function handleSignOut() {
-    try {
-        await signOut();
-        console.log('Signed out');
-    } catch (error) {
-        console.error('Error signing out: ', error);
-    }
-}
-
-handleSignIn();
+export default App;
